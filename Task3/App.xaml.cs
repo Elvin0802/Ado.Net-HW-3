6789802b1e14785779b0ef952e905694098c0cbc
Elvin0802 <elvincode1517@gmail.com>
2024-07-16 16:03:44 +0400

    Adding Project

---
+++ Task3/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Windows;
+using Task3.Views;
+
+namespace Task3;
+
+public partial class App : Application
+{
+	public static IConfigurationRoot? Configuration { get; set; }
+
+	private void AppStartup(object sender, StartupEventArgs e)
+	{
+		Configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("Resources/appsettings.json")
+				.Build();
+
+
+		var win = new MainWindow();
+
+		win.ShowDialog();
+
+	}
+}
---
+++ Task3/Views/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Task3.Views;
+
+public partial class MainWindow : Window
+{
+	public SqlConnection? Conn { get; set; }
+	public SqlDataAdapter? Adapter { get; set; }
+	public DataSet? DataSet { get; set; }
+	public string? ConnStr { get; set; }
+
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		ConnStr = App.Configuration!.GetConnectionString("DefaultConnection")!;
+
+		AuthorsNameCBox.ItemsSource = GetAuthorsName();
+
+		AuthorsNameCBox.Items.Refresh();
+	}
+
+	private List<string>? GetAuthorsName()
+	{
+		List<string>? Authors = new();
+
+		DataSet = new();
+
+		string? query = $@"
+							SELECT [FirstName], [LastName]
+							FROM Authors
+		";
+
+		Conn = new(ConnStr);
+
+		Adapter = new(query, Conn);
+
+		Adapter.Fill(DataSet, "authors");
+
+		foreach (DataRow row in DataSet.Tables["authors"]!.Rows)
+		{
+			Authors.Add($"{row["FirstName"]} | {row["LastName"]}");
+		}
+
+		return Authors;
+	}
+
+	private void NewAuthorSelected(object sender, SelectionChangedEventArgs e)
+	{
+		DataSet = new();
+
+		string? fn = $"{AuthorsNameCBox.SelectedValue.ToString()!.Split('|')[0]}",
+					ln = $"{AuthorsNameCBox.SelectedValue.ToString()!.Split('|')[1]}";
+
+		string? query = $@"
+								SELECT [Name], [Pages], [YearPress], [Comment], [Quantity]   
+								FROM Authors INNER JOIN Books
+								ON Authors.Id = Books.Id_Author
+								WHERE Authors.[FirstName] IN(N'{fn}') OR Authors.[LastName] IN(N'{ln}')
+		";
+
+		Conn = new(ConnStr);
+
+		Adapter = new(query, Conn);
+
+		Adapter.Fill(DataSet, "books");
+
+		BooksDG.ItemsSource = DataSet.Tables["books"]!.DefaultView;
+		BooksDG.Items.Refresh();
+	}
+}
